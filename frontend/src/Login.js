74af3fa1b206fae2a2a847d55039ad466cdecee0
Crystal Merlin D Souza <crystalmdsouza18@gmail.com>
2025-03-08 10:37:14 +0530

    login created

---
+++ frontend/src/Login.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'; 
+import {TextField,Button,Page} from '@mui/material';
+
+function Login() {
+    return (
+  <div>
+    <Page>
+        <TextField label="Name"></TextField>
+    </Page>
+  </div>
+    );
+  }
+  
+  export default Login;
+  
